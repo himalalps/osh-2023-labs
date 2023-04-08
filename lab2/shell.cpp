5c1e265741e194c0e2a3135a4f97fdb5fadbdcf9
hao <haoyu_t@mail.ustc.edu.cn>
2023-04-08 18:28:25 +0800

    pipe

--- lab2/shell.cpp
+++ lab2/shell.cpp
@@ -20,6 +20,7 @@ void trim(std::string &line);
 void format(std::string &line);
 void pwd(const std::vector<std::string> &args);
 void cd(std::vector<std::string> &args, const char *home);
+void preprocess(std::vector<int> &poses, std::vector<std::string> &args, char **arg_ptrs, int &pipe_num, const char *home);
 
 int main() {
     // 不同步 iostream 和 cstdio 的 buffer
@@ -90,64 +91,52 @@ int main() {
         int pipe_num = 0;
         // 管道对应位置
         std::vector<int> poses;
-        poses.push_back(-1);
-        for (auto i = 0; i < args.size(); i++) {
-            if (args[i].substr(0, 1) == "$") { // 替换环境变量
-                args[i].replace(0, args[i].length(), getenv(&(args[i].substr(1, args[i].length() - 1)[0])));
-            } else if (args[i].substr(0, 2) == "~/" || args[i] == "~") { // 替换 ~
-                args[i].replace(0, 1, std::string(home));
-            }
-            if (args[i] == "|") {
-                pipe_num++;
-                poses.push_back(i);
-                arg_ptrs[i] = nullptr;
-            } else {
-                arg_ptrs[i] = &args[i][0];
-            }
-        }
-        // exec p 系列的 argv 需要以 nullptr 结尾
-        arg_ptrs[args.size()] = nullptr;
+
+        // 预处理
+        preprocess(poses, args, arg_ptrs, pipe_num, home);
 
         // 处理外部命令
-        pid_t pid = fork();
-
-        if (pid < 0) {
-            std::cout << "fork failed" << std::endl;
-        } else if (pid == 0) {
-            // 这里只有子进程才会进入
-            if (pipe_num > 0) { // 管道
-                int pipefd[pipe_num][2];
-                for (int i = 0; i < pipe_num; i++) {
-                    if (pipe(pipefd[i]) == -1) {
-                        std::cout << "pipe failed" << std::endl;
-                        exit(255);
-                    }
+        if (pipe_num > 0) {
+            // 存在管道
+            int pipefd[pipe_num][2];
+            for (int i = 0; i < pipe_num; i++) {
+                if (pipe(pipefd[i]) == -1) {
+                    std::cout << "pipe failed" << std::endl;
+                    exit(255);
                 }
-                for (int i = 0; i < pipe_num + 1; i++) {
-                    pid_t cpid = fork();
-                    if (cpid < 0) {
-                        std::cout << "fork failed" << std::endl;
-                        exit(255);
-                    } else if (cpid == 0) {
-                        // 输出重定向
-                        if (i == 0) { // 第一个子进程输入来自标准输入
-                            dup2(pipefd[i][1], STDOUT_FILENO);
-                            close(pipefd[i][1]);
-                        } else if (i == pipe_num) { // 最后一个子进程输出到标准输出
-                            dup2(pipefd[i - 1][0], STDIN_FILENO);
-                            close(pipefd[i - 1][0]);
-                        } else { // 其余进程输入输出都是管道
-                            dup2(pipefd[i - 1][0], STDIN_FILENO);
-                            dup2(pipefd[i][1], STDOUT_FILENO);
-                            close(pipefd[i - 1][0]);
-                            close(pipefd[i][1]);
-                        }
-
-                        execvp(args[poses[i] + 1].c_str(), &arg_ptrs[poses[i] + 1]);
-                        exit(255);
+            }
+            for (int i = 0; i < pipe_num + 1; i++) {
+                pid_t cpid = fork();
+                if (cpid < 0) {
+                    std::cout << "fork failed" << std::endl;
+                    exit(255);
+                } else if (cpid == 0) {
+                    // 输出重定向
+                    if (i == 0) { // 第一个子进程输入来自标准输入
+                        dup2(pipefd[i][1], STDOUT_FILENO);
+                        close(pipefd[i][1]);
+                    } else if (i == pipe_num) { // 最后一个子进程输出到标准输出
+                        dup2(pipefd[i - 1][0], STDIN_FILENO);
+                        close(pipefd[i - 1][0]);
+                    } else { // 其余进程输入输出都是管道
+                        dup2(pipefd[i - 1][0], STDIN_FILENO);
+                        dup2(pipefd[i][1], STDOUT_FILENO);
+                        close(pipefd[i - 1][0]);
+                        close(pipefd[i][1]);
                     }
+
+                    execvp(args[poses[i]].c_str(), &arg_ptrs[poses[i]]);
+                    exit(255);
                 }
-            } else {
+            }
+        } else {
+            // 不存在管道
+            pid_t pid = fork();
+
+            if (pid < 0) {
+                std::cout << "fork failed" << std::endl;
+            } else if (pid == 0) {
+                // 这里只有子进程才会进入
                 // execvp 会完全更换子进程接下来的代码，所以正常情况下 execvp 之后这里的代码就没意义了
                 // 如果 execvp 之后的代码被运行了，那就是 execvp 出问题了
                 execvp(args[0].c_str(), arg_ptrs);
@@ -242,5 +231,26 @@ void cd(std::vector<std::string> &args, const char *home) {
     if (chdir(args[1].c_str()) == -1) {
         std::cout << "cd failed" << std::endl;
     }
+    return;
+}
+
+void preprocess(std::vector<int> &poses, std::vector<std::string> &args, char **arg_ptrs, int &pipe_num, const char *home) {
+    poses.push_back(0);
+    for (auto i = 0; i < args.size(); i++) {
+        if (args[i].substr(0, 1) == "$") { // 替换环境变量
+            args[i].replace(0, args[i].length(), getenv(&(args[i].substr(1, args[i].length() - 1)[0])));
+        } else if (args[i].substr(0, 2) == "~/" || args[i] == "~") { // 替换 ~
+            args[i].replace(0, 1, std::string(home));
+        }
+        if (args[i] == "|") {
+            pipe_num++;
+            poses.push_back(i + 1);
+            arg_ptrs[i] = nullptr;
+        } else {
+            arg_ptrs[i] = &args[i][0];
+        }
+    }
+    // exec p 系列的 argv 需要以 nullptr 结尾
+    arg_ptrs[args.size()] = nullptr;
     return;
 }
