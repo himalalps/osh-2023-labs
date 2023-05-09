ba025e568289c5820076b47deb1fbc16cd762718
hao <haoyu_t@mail.ustc.edu.cn>
2023-05-09 22:27:17 +0800

    reorganize

--- lab3/server_thread_pool/src/main.rs
+++ lab3/server_thread_pool/src/main.rs
@@ -1,11 +1,15 @@
+pub mod pool;
+
+use pool::ThreadPool;
 use server::*;
-use std::{net::TcpListener, thread};
+use std::net::TcpListener;
 
 const BIND_IP: &str = "0.0.0.0:8000";
 
 fn main() {
     let listener =
         TcpListener::bind(BIND_IP).expect(format!("Failed to bind to {}.", BIND_IP).as_str());
+    let pool = ThreadPool::new(32);
 
     for stream in listener.incoming() {
         let stream = match stream {
@@ -16,7 +20,7 @@ fn main() {
             }
         };
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
